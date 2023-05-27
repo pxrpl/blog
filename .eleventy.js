46b753892d64966ee458d37401dc8058a51ac581
Vicent <87479344+pxrpl@users.noreply.github.com>
2023-05-27 13:40:07 +0200

    Fixed admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
    
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addPassthroughCopy("./src/images");
+    eleventyConfig.addPassthroughCopy("./src/admin");
     eleventyConfig.addWatchTarget("./src/css/");
     eleventyConfig.addWatchTarget("./src/images/");
 
